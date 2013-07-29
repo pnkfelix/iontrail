96d52a89cd5426782c07617ec84487fc8f8902d0
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-07-29 15:22:53 +0200

    Manually cloning the test driving routine via cut-and-paste.
    Manually cloning the code since parallization will currently fail if
    you attempt to parameterize over distinct typed-array consructor.
    
    (We are discussing removing that latter limitation by adding an
    inline-cache for UnsafePutElements, analogously to how we have one for
    get operations.

--- js/src/jit-test/tests/parallel/matrix-construct-1d.js
+++ js/src/jit-test/tests/parallel/matrix-construct-1d.js
@@ -53,34 +53,295 @@ Uint32Array.build = buildArray;
 Float32Array.build = buildArray;
 Float64Array.build = buildArray;
 
+
 function constructInt8() {
-  function kernel(i) { return i+200; };
-  var m = new Matrix([512], ["int8"], kernel);
-  var a = Int8Array.build(512, function (i) { return i+200; });
-  assertEqMatrixTypedArray(m, a, Int8Array);
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
 
-  assertParallelModesCommute(function(m) {
-    return new Matrix([512], ["int8"], kernel, m);
-  });
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Int8Array, "int8");
 }
-
 function constructInt8Out() {
-  function kernel(i, out) { out.set(i+200); };
-  var m = new Matrix([512], ["int8"], kernel);
-  var a = Int8Array.build(512, function (i) { return i+200; });
-  assertEqMatrixTypedArray(m, a, Int8Array);
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
 
-  assertParallelModesCommute(function(m) {
-    return new Matrix([512], ["int8"], kernel, m);
-  });
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Int8Array, "int8");
 }
 
+function constructUint8() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Uint8Array, "uint8");
+}
+function constructUint8Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Uint8Array, "uint8");
+}
+
+function constructUint8clamped() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Uint8ClampedArray, "uint8clamped");
+}
+function constructUint8clampedOut() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Uint8ClampedArray, "uint8clamped");
+}
+
+function constructInt16() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Int16Array, "int16");
+}
+function constructInt16Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Int16Array, "int16");
+}
+
+function constructUint16() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Uint16Array, "uint16");
+}
+
+function constructUint16Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Uint16Array, "uint16");
+}
+
+function constructInt32() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Int32Array, "int32");
+}
+
+function constructInt32Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Int32Array, "int32");
+}
+
+function constructUint32() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Uint32Array, "uint32");
+}
+
+function constructUint32Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Uint32Array, "uint32");
+}
+
+function constructFloat32() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+  
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Float32Array, "float32");
+}
+
+function constructFloat32Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Float32Array, "float32");
+}
+
+function constructFloat64() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTyped(AT, ATdesc) {
+    function kernel(i) { return i+200; };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTyped(Float64Array, "float64");
+}
+
+function constructFloat64Out() {
+  // e.g. AT == Int8Array, ATdesc == "int8"
+  function constructTypedOut(AT, ATdesc) {
+    function kernel(i, out) { out.set(i+200); };
+    var m = new Matrix([512], [ATdesc], kernel);
+    var a = AT.build(512, function (i) { return i+200; });
+    assertEqMatrixTypedArray(m, a, AT);
+
+    assertParallelModesCommute(function(m) {
+      return new Matrix([512], [ATdesc], kernel, m);
+    });
+  }
+
+  constructTypedOut(Float64Array, "float64");
+}
 
 try {
   if (getBuildConfiguration().parallelJS) {
     constructAny();
     constructInt8();
-/*
     constructUint8();
     constructUint8clamped();
     constructInt16();
@@ -89,11 +350,9 @@ try {
     constructUint32();
     constructFloat32();
     constructFloat64();
-*/
 
     constructAnyOut();
     constructInt8Out();
-/*
     constructUint8Out();
     constructUint8clampedOut();
     constructInt16Out();
@@ -102,7 +361,6 @@ try {
     constructUint32Out();
     constructFloat32Out();
     constructFloat64Out();
-*/
   }
 } catch (e) {
   print(e.name);
