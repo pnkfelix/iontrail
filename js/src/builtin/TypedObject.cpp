86fe21c726cd69087cb4b7c4834221d1465c42b7
Niko Matsakis <niko@alum.mit.edu>
2013-09-20 16:19:37 -0400

    Add dimension method

--- js/src/builtin/TypedObject.cpp
+++ js/src/builtin/TypedObject.cpp
@@ -500,6 +500,7 @@ const JSPropertySpec ArrayType::typedObjectProperties[] = {
 const JSFunctionSpec ArrayType::typedObjectMethods[] = {
     JS_FN("subarray", ArrayType::subarray, 2, 0),
     {"forEach", {NULL, NULL}, 1, 0, "ArrayForEach"},
+    {"redimension", {NULL, NULL}, 1, 0, "TypedArrayRedimension"},
     JS_FS_END
 };
 
