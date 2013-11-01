dbb4714aa6bc60cbc27a3d0665b3eecea841241a
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-11-01 17:08:00 +0100

    all fsk tests for TypedObject array-access.

--- js/src/jit-test/tests/TypedObject/jit-read-float64.js
+++ js/src/jit-test/tests/TypedObject/jit-read-float64.js
@@ -9,7 +9,7 @@ function foo() {
   for (var i = 0; i < 30000; i += 3) {
     var pt = new PointType({x: i, y: i+1, z: i+2});
     var sum = pt.x + pt.y + pt.z;
-    print(pt.x, pt.y, pt.z);
+    // print(pt.x, pt.y, pt.z);
     assertEq(sum, 3*i + 3);
   }
 }
--- js/src/jit-test/tests/TypedObject/jit-read-int-from-int-array.js
+++ js/src/jit-test/tests/TypedObject/jit-read-int-from-int-array.js
@@ -1,14 +1,26 @@
 if (!this.hasOwnProperty("TypedObject"))
   quit();
 
-var Vec3Type = TypedObject.uint32.array(3);
+var Vec3u16Type = TypedObject.uint16.array(3);
 
-function foo() {
+function foo_u16() {
   for (var i = 0; i < 30000; i += 3) {
-    var vec = new Vec3Type([i, i+1, i+2]);
+    var vec = new Vec3u16Type([i, i+1, i+2]);
     var sum = vec[0] + vec[1] + vec[2];
     assertEq(sum, 3*i + 3);
   }
 }
 
-foo();
+foo_u16();
+
+var Vec3u32Type = TypedObject.uint32.array(3);
+
+function foo_u32() {
+  for (var i = 0; i < 30000; i += 3) {
+    var vec = new Vec3u32Type([i, i+1, i+2]);
+    var sum = vec[0] + vec[1] + vec[2];
+    assertEq(sum, 3*i + 3);
+  }
+}
+
+foo_u32();
